8daed38f34b4bef050d978d2fa3037b1112bd131
Anny0810 <angel.hwg@gmail.com>
2025-12-31 15:13:53 +0800

    55555

--- server.js
+++ server.js
@@ -17,30 +17,13 @@ var DB=require("nedb-promises");
 var ServiceDB = DB.create(__dirname+"/Service.db");
 var PorfolioDB = DB.create(__dirname+"/Porfolio.db");
 var ContactDB = DB.create(__dirname+"/Contact.db");
-// ServiceDB.insert([
-//         { icon: 'fa-shopping-cart', title: 'E-Commerce', text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur porro laborum fuga repellat necessitatibus corporis nulla, in ex velit recusandae obcaecati maiores, doloremque quisquam similique, tempora aspernatur eligendi delectus! Rem.' },
-//         { icon: 'fa-laptop', title: 'Responsive Design', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.' },
-//         { icon: 'fa-lock', title: 'Web Security', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.' }
-//     ])
-//  PorfolioDB.insert( [
-//         { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
-//         { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
-//         { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" },
-//         { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
-//         { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
-//         { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" }
-//     ])
+
 
 server.get("/", (req, res) => {
     res.send("Hello world!");
 })
 server.get("/services", (req, res) => {
-    //db 
-    // var Services = [
-    //     { icon: 'fa-shopping-cart', title: 'E-Commerce', text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur porro laborum fuga repellat necessitatibus corporis nulla, in ex velit recusandae obcaecati maiores, doloremque quisquam similique, tempora aspernatur eligendi delectus! Rem.' },
-    //     { icon: 'fa-laptop', title: 'Responsive Design', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.' },
-    //     { icon: 'fa-lock', title: 'Web Security', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.' }
-    // ]
+    
     ServiceDB.find({},{_id:0}).then(results=>{
        
         res.send(results);
@@ -51,14 +34,7 @@ server.get("/services", (req, res) => {
 })
 
 server.get("/portfolio", (req, res) => {
-    // var Portfolio = [
-    //     { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
-    //     { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
-    //     { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" },
-    //     { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
-    //     { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
-    //     { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" }
-    // ]
+
     PorfolioDB.find({}).then(results=>{
         res.send(results);
     })
@@ -66,15 +42,7 @@ server.get("/portfolio", (req, res) => {
 })
 
 
-server.get("/showServices",(req,res)=>{
-    ServiceDB.find({},{_id:0}).then(results=>{
-       
-        res.render("service",{Services:results});
-    }).catch(error=>{
 
-    })
-
-})
 
 server.get("/about", (req, res) => {
     res.send("Welcome " + req.query.user + " to My first NodeJS server!");
@@ -101,4 +69,4 @@ server.post("/contact", (req, res) =>{
 
 
 
-server.listen(80)
+server.listen(8080)
